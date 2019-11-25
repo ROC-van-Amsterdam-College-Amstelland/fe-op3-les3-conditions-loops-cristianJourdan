a208c595a71924f9241b8fd1f0aaafbb320d238f
cristian Jourdan <2106549@talnet.nl>
2019-11-25 14:54:49 +0100

    opdracht 2 condities

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,10 @@
-//javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value
+
+if(saldo >= 25){
+    document.getElementById("tekst").innerHTML ="je hebt voldoende saldo om 25 euro op te nemen"
+}else{
+    document.getElementById("tekst").innerHTML="Helaas, u heeft te weinig geld!"
+}
+
+}
