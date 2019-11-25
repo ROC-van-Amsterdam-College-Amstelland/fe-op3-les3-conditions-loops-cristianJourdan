c547082683b83b42d53459434b4bcf45cfbae6b5
cristian Jourdan <2106549@talnet.nl>
2019-11-25 14:50:16 +0100

    opdr 1 condities

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,10 @@
-//javascript code
+function check() {
+        var saldo = document.getElementById("saldo").value
+
+    if(saldo<25){
+        document.getElementById("tekst").innerHTML ="Helaas, u heeft te weinig geld!"
+    }else{
+        document.getElementById("tekst").innerHTML="."
+    }
+
+}
