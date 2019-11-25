48f446940aef6a259b5da94989ff92f81e4008a4
cristian Jourdan <2106549@talnet.nl>
2019-11-25 15:06:49 +0100

    opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,12 @@
-//javascript code
+function check() {
+     var saldo = document.getElementById("saldo").value
+
+    if(saldo == 500){
+        document.getElementById("tekst").innerHTML="salaris boiis"
+    }else if(saldo>500){
+        document.getElementById("tekst").innerHTML= "Bonus BOII FEESIEE"
+    }else{
+        document.getElementById("tekst").innerHTML= "Helaas! u bent skheer!"  
+    }
+
+}
