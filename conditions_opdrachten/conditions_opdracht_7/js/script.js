7b1942c7b6fb0b440df33d03818b81153ff4067e
cristian Jourdan <2106549@talnet.nl>
2019-11-26 13:37:20 +0100

    opdr 7 maar hoeft blijkbaar niet

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -1,1 +1,8 @@
-//javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value
+
+   if(saldo == 1){
+       document.getElementById("body").style.backgroundColor= "red";
+       document.getElementById("tekst").innerHTML= "DIRTY BOII"
+   }
+}
