745f8b7535041b3baad9da753736022f6ca50276
cristian Jourdan <2106549@talnet.nl>
2019-11-26 13:21:35 +0100

    opdr 5

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,4 +1,15 @@
- var myUitkomst = MyNumber1 + MyOperator + MyNumer2 
- function myUitkomst(){
+calculate = function(keer)
+{
+    var getal1 = document.getElementById('a1').value;
+    var getal2 = document.getElementById('a2').value; 
+    document.getElementById('a3').value = parseInt(getal1) * parseInt(getal2);
      
- }
+   }
+
+calculate2 = function(delen)
+   {
+       var getal3 = document.getElementById('b1').value;
+       var getal4 = document.getElementById('b2').value; 
+       document.getElementById('b3').value = parseInt(getal3) / parseInt(getal4);
+        
+      }
