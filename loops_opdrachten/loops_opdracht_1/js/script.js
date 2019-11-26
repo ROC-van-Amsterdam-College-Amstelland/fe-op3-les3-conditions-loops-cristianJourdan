5d2f47f8ba61f2507c6081bb9de8f16e9a2adc0b
cristian Jourdan <2106549@talnet.nl>
2019-11-26 13:48:49 +0100

    loops opdr 1

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,7 @@
-//javascript code
+var getallen = ["1", "2", "3", "4", "5"];
+var text = "";
+var i;
+for (i = 0; i < getallen.length; i++) {
+  text += getallen[i] + "<br>";
+}
+document.getElementById("loop").innerHTML = text;
