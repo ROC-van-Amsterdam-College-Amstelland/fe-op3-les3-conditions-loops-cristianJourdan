4725d318503c3e238753b7fd09d2a8441bfc7b55
cristian Jourdan <2106549@talnet.nl>
2019-12-03 16:02:40 +0100

    loops opdr 4

--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,1 +1,9 @@
-//javascript code
+var tekst="";
+for(var i = 10; i <= 500; i += 10) {
+    if (i==20 || i==40) {
+        tekst +=i + ",dit is een mooi getal <br>";
+    } else {
+        tekst += i + "<br>";
+    }
+}
+document.getElementById("bok").innerHTML = tekst;
